254b06d96997535eee8ea698ba7aa1c624a790bd
Ajat Darojat <ajatdarojat45@gmail.com>
2021-04-27 21:34:40 +0700

    sesi 13

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import ListItem from "./components/ListItems";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <h1>React Testing</h1>
       <ListItem items={["Orange", "Apple", "Pear"]} />
+      <Counter />
     </div>
   );
 }
